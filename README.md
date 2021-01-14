79c611b94c94ed77ff429314405e32df475f5449
Vishakh Padmakumar <vp1271@log-2.nyu.cluster>
2021-01-14 10:48:30 -0500

    PMI matrices

--- README.md
+++ README.md
@@ -11,4 +11,37 @@
 
 We use GPT2-large as provided by [HF transformers](https://github.com/huggingface/transformers) with a standard fine tuning script as provided in the model directory.
 
+### Creating mutual information matrices 
+
+For each document in the dataset, if there are n sentences, we need to create the nxn pairwise matrix as described in the paper. <br/>
+
+```
+python get_matrices.py --index 0 --input_data ../../Summarization/dataset.pkl --index_file ../../Summarization/indices.pkl --output_format ../../Summarization/output/sent_train_set_
+```
+
+The parameters indicate the following:
+1. input\_data - Pickle file containing the document-summary files. Check that the format matches the following input scheme: 
+```
+data = pickle.load(open(args.input_data, "rb"))
+for i in range(len(data)):
+    article, abstract = data[i]
+```
+2. index\_file - Since the process takes a while, we split the same into different parts by means of a pickled index file. The index file consists of a list of tuples of (start index, end index) within the indexing of the length of the dataset file. So if the dataset is of length 100, our index file might be [(0, 30), (30, 60), (60, 90), (90, 100)] and each execution of get\_matrices with a particular index creates the matrices associated with those datapoints in the dataset. The simplest way to handle this would be to make a list with just one tuple as [(0, length of dataset)].
+3. index - Index within the index file to execute. Index file is a list, so if index is 0, then the matrices are created for the documents associated with the indices enclosed by the first tuple in the index file. So you can parallelize the process by running multiple at the same time. 
+4. output\_format - File format/location where the output is stored. Output is stored in the form of a pickle file at output format location with the index appended at the end. 
+
+#### Output Format
+For the given execution line, the output would look like:
+```
+../../Summarization/output/sent_train_set_0.pkl
+```
+Where the location/directory was given by the output format and the 0 is given by the index executed by the current script (in practise you would have 0, 1, 2... all running in parallel)<br/>
+
+The pickle file is indexed as 
+```
+data = pickle.load(open("../../Summarization/output/sent_train_set_0.pkl", "rb"))
+doc_number = 0
+print(data[doc_number]['vanilla'])
+```
+Within the pickle file, the primary index is the index of the document within the dataset pickle file. We create the 'vanilla' matrix which is an nxn sentence-wise PMI matrix as described in the paper. Additionally we also create the unused 'surprise' list which is the sentence-wise surprisal for each sentence and the 'normalised' PMI matrix.
 
---
+++ get_matrices.py
@@ -0,0 +1,242 @@
+import os
+import sys
+sys.path.insert(0, "./models/")
+sys.path.insert(0, "./read-data/")
+from read import get_text
+from gpt2 import GPT2
+from tqdm import tqdm
+import preprocess_subsequence
+import nltk
+import math
+from nltk import tokenize
+import numpy as np
+#from sklearn.metrics import precision_score, recall_score
+import pickle
+import spacy
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--index")
+parser.add_argument("--input_data")
+parser.add_argument("--index_file")
+parser.add_argument("--output_format")
+args = parser.parse_args()
+print(args.index)
+print(args.index_file)
+print(args.input_data)
+print(args.output_format)
+
+data = pickle.load(open(args.input_data, "rb"))#
+print("Length ", len(data))
+indices = pickle.load(open(args.index_file, "rb"))
+print(indices)
+lower, upper = indices[int(args.index)]
+print(lower, upper)
+
+nlp = spacy.load("en_core_web_sm")
+
+model = GPT2(device="cuda", location="../../Summarization/models/")
+
+def get_probabilities(articles):
+    article_splits = [article.split(" ") for article in articles]
+    payload = model.get_probabilities(articles, topk = 20)
+    res = [[] for i in range(len(articles))]
+    for t, article in enumerate(articles):
+        context = ""
+        idx = 0
+        chain = False
+        next_word = ""
+        article_words = article_splits[t]
+        #print(article, article_words)
+        word_probability = 1.0
+        gt_count = 0
+        idx+=1
+        found_words = []
+        for i, word in enumerate(payload["context_strings"][t][:-1]):
+            context = context+" "+word
+            #print("\n--------------------\n", word, word[0]=="Â")
+            #predictions = payload["pred_topk"][t][i]
+            #position = payload['real_probs'][t][i][0] #Position of correct guess within predictions
+            probability = payload['real_probs'][t][i]#[1]
+            next_word_fragment = payload["context_strings"][t][i+1]
+
+            next_word += next_word_fragment
+            #print(next_word, article_words[gt_count])
+            if next_word == article_words[gt_count]:
+                chain = False
+                gt_count+=1
+            else:
+                chain = True
+
+            word_probability *= probability
+            assert word_probability <= 1.0, print(word_probability, context)
+            if chain == False:      
+                #print("Word Probability: ", word_probability, next_word)
+                res[t].append(word_probability)
+                word_probability = 1.0 
+                next_word = ""
+            #print(gt_count, len(article_words))
+            if gt_count == len(article_words):
+                break
+    return res
+
+
+def get_pmi_matrix(sentences, method = 1, batch_size = 20):
+    temp = np.zeros((len(sentences), len(sentences)))
+    batch_indices = {}
+    batch = []
+    batchCount = 0
+    batchSize = batch_size
+    #print(len(sentences))
+    for i in range(len(sentences)):
+        for j in range(len(sentences)):
+            #print(i, j)
+            if i==j: 
+                temp[i][j] = -1
+                continue
+            article = sentences[i] + " "+ sentences[j]
+            #print(article)
+            batch_indices[str(i)+"-"+str(j)+"-"+str(len(sentences[i].split()))] = [batchCount, batchCount+1] 
+            batch.append(article)
+            batch.append(sentences[j])
+            batchCount+=2
+            
+            if batchCount == batchSize or (i == len(sentences)-1 and j == len(sentences)-1):
+                #print(batch)
+                result = get_probabilities(batch)
+                for key in batch_indices.keys():
+                    #print(key)
+                    #print(key.split("-"))
+                    i, j, l = [int(idx) for idx in key.split("-")]
+                    try:
+                        temp[i][j] = (sum([math.log(i) for i in result[batch_indices[key][0]][l:]]) - sum([math.log(i) for i in result[batch_indices[key][1]]]))/(-1*sum([math.log(i) for i in result[batch_indices[key][0]][l:]]))
+                    except ZeroDivisionError:
+                        temp[i][j] = -1
+                    #temp[i][j] = sum([math.log(i) for i in result[batch_indices[key][0]][l:]]) - sum([math.log(i) for i in result[batch_indices[key][1]]])
+                batchCount = 0
+                batch = []
+                batch_indices = {}
+    return temp
+
+def get_npmi_matrix(sentences, method = 1, batch_size = 1):
+    temp = np.zeros((len(sentences), len(sentences)))
+    temp2 = np.zeros((len(sentences), len(sentences)))
+    batch_indices = {}
+    batch = []
+    batchCount = 0
+    batchSize = batch_size
+    #print(len(sentences))
+    c = 0
+    p = []
+    for i in range(len(sentences)):
+        result = get_probabilities([sentences[i]])
+        try:
+            p.append(sum([math.log(i) for i in result[0]]))
+        except:
+            print("Math domain error surprise", i)
+            return temp, temp2, p
+    for i in range(len(sentences)):
+        for j in range(len(sentences)):
+            #print(i, j)
+            if i==j:# len(sentences[i].split()) == 0 or len(sentences[j].split()) == 0 or 
+                temp[i][j] = -1
+                temp2[i][j] = -1
+                continue
+            article = sentences[i] + " "+ sentences[j]
+            #print(article)
+            batch_indices[str(i)+"-"+str(j)+"-"+str(len(sentences[i].split()))] = batchCount#[batchCount, batchCount+1, batchCount+2] 
+            batch.append(article)
+            #batch.append(sentences[j])
+            #batch.append(sentences[i])
+            batchCount+=1
+            
+            if batchCount == batchSize or (i == len(sentences)-1 and j == len(sentences)-1):
+                #print(batch)
+                c+=1
+                result = get_probabilities(batch)
+                for key in batch_indices.keys():
+                    #print(key)
+                    #print(key.split("-"))
+                    idx_i, idx_j, idx_l = [int(idx) for idx in key.split("-")]
+                    try:
+                        pxy = sum([math.log(q) for q in result[batch_indices[key]][idx_l:]])
+                        py = p[idx_j]
+                        px = p[idx_i]
+                    
+                        temp[idx_i][idx_j] = (pxy - py)/(-1*(pxy+px))
+                        temp2[idx_i][idx_j] = (pxy - py)
+                    except ZeroDivisionError:
+                        print("Zero division error ", idx_i, idx_j)
+                        temp[idx_i][idx_j] = -1
+                        temp2[idx_i][idx_j] = -1
+                    except:
+                        print("Math Domain Error", i, j)
+                    if temp[idx_i][idx_j] > 1 or temp[idx_i][idx_j] < -1:
+                        print("Normalise assert ", temp[idx_i][idx_j], idx_i, idx_j)
+                batchCount = 0
+                batch = []
+                batch_indices = {}
+    return temp, temp2, p
+
+
+def get_sentence_groundtruths(article, sample_groundtruths):
+    article_sents = tokenize.sent_tokenize(article)
+    start = 0
+    end = 0
+    res_groundtruths = []
+    for sent in article_sents:
+        end+=len(sent.split())
+        if sum(sample_groundtruths[start:end]) > 0:
+            res_groundtruths.append(1)
+        else:
+            res_groundtruths.append(0)
+        start = end
+    return res_groundtruths
+
+def get_topk(temp, k=5):
+    max_idxs = [np.argsort(i) for i in temp]
+    sums = []
+    for i in range(len(temp)):
+        s=0
+        for idx in max_idxs[i]:
+            s+=temp[i][idx]
+        sums.append(s)
+    #[print(i, sums[i]) for i in np.argsort(sums)[-k:]]
+    sents = [0 for i in range(len(temp))]
+    for i in np.argsort(sums)[-k:]:
+        sents[i]=1
+    return [sentences[i] for i in sorted(np.argsort(sums)[-k:])], sents
+
+
+if os.path.exists(args.output_format+args.index+".pkl"):
+    output = pickle.load(open(args.output_format+args.index+".pkl","rb"))
+else:
+    output = {}
+
+def remove_unicode(text):
+    return ''.join([i if ord(i) < 128 else ' ' for i in text])
+
+def get_article(idx):
+    print(idx)
+    article, abstract = data[idx]
+    #sentences = tokenize.sent_tokenize(article)
+    doc = nlp(article)
+    sentences = [remove_unicode(sentence.text) for sentence in doc.sents]
+    normalised, vanilla, surprise = get_npmi_matrix(sentences, batch_size = 10)
+    #avg = get_pmi_matrix(sentences, method = 1)
+    output[idx] = {}
+    output[idx]["vanilla"] = vanilla
+    output[idx]["normalised"] = normalised
+    output[idx]["surprise"] = surprise
+    #output[idx]["averaging"] = avg
+    #pickle.dump(output, open("full_set_1.pkl", "wb"))
+    return
+
+c = 0
+for idx in range(len(data)):
+    if idx>=lower and idx<upper and idx not in output.keys():
+        get_article(idx) 
+    if c%20 == 0:
+        pickle.dump(output, open(args.output_format+args.index+".pkl", "wb"))
+    c+=1        
+
+pickle.dump(output, open(args.output_format+args.index+".pkl", "wb"))
